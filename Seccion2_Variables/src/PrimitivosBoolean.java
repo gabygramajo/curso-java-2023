e7401d399c864eab7e1a2f34fa7c9514eaa73ca7
Gaby <ggabrieldeveloper@gmail.com>
2023-10-03 20:42:58 -0300

    add: operadores

--- Seccion2_Variables/src/PrimitivosBoolean.java
+++
@@ -1,9 +0,0 @@
-public class PrimitivosBoolean {
-    public static void main(String[] args) {
-        System.out.println("Datos Primitivos - BOOLEAN");
-
-        //Boolean
-        boolean nroBoolean = true;
-        System.out.println("\nnroBoolean = " + nroBoolean);
-    }
-}

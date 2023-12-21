c6b7617ebfccb9df339949a15bcb0f0860f1e2d1
Gabriel Gramajo <77758104+gabygramajo@users.noreply.github.com>
2023-12-21 09:18:27 -0300

    validar Strings

--- FundamentosJava/src/ValidarString.java
+++ FundamentosJava/src/ValidarString.java
@@ -1,30 +1,27 @@
 public class ValidarString {
+
     public static void main(String[] args) {
-        String curso = null;
-
-        boolean esNulo = curso == null;
-        System.out.println("es null: " + esNulo);
-        //>> true
-
-        //prevenir error con string nulo
-        if(!esNulo) {
-            System.out.println(curso.toUpperCase());
+        String resultado;
+        String text = null;
+        if (!(text == null)) {
+            resultado = "Es nulo";
+        } else {
+            resultado = "No es nulo";
         }
-        //cuando un valor es nulo, lanza la excepcion de "NullPointerException"
 
-        //String vacio
-        String str1 = "";
-        System.out.println("1- str1 es igual null:" + (str1 == null)); // >> false
-        System.out.println("2- str1 es vacio:" + (str1.length() == 0)); // >> true
-        System.out.println("3- str1 es vacio:" + str1.isEmpty()); // >> >> true
+        System.out.println("resultado = " + resultado);
 
-        String str2 = " ";
-        System.out.println("4- str2 es igual null:" + (str2 == null)); // >> false
-        System.out.println("5- str2 es vacio:" + (str2.length() == 0)); // >> false
-        System.out.println("6- str2 es vacio:" + str2.isEmpty()); // >> >> false
+        text = "";
+        System.out.println("text isEmpty = " + text.isEmpty());
+        System.out.println("text isBlank = " + text.isBlank());
+//        text isEmpty = true
+//        text isBlank = true
 
-        str2 = str2.trim();
-        System.out.println("7- str2 es vacio:" + (str2.length() == 0)); // >> true
-        System.out.println("8- str2 es vacio:" + str2.isEmpty()); // >> >> true
+        text = " ";
+        System.out.println("text isEmpty = " + text.isEmpty());
+        System.out.println("text isBlank = " + text.isBlank());
+//        text isEmpty = false
+//        text isBlank = true
+
     }
 }
